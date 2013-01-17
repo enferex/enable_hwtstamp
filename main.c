0c8d8ff7478985259c04aec84d827b4af8821103
Matt Davis <mattdavis9@gmail.com>
2013-01-18 10:25:55 +1100

    Fix spacing

--- main.c
+++ main.c
@@ -13,31 +13,31 @@
 /* Enable hardware timestamping on all packets received */
 static void set_settings(int sd, const char *iface)
 {
-	struct ifreq dev;
-	struct hwtstamp_config cfg;
+    struct ifreq dev;
+    struct hwtstamp_config cfg;
 
     memset(&dev, 0, sizeof(struct ifreq));
     memset(&cfg, 0, sizeof(struct hwtstamp_config));
 
-	cfg.rx_filter = HWTSTAMP_FILTER_ALL;
-	cfg.tx_type = HWTSTAMP_TX_OFF;
+    cfg.rx_filter = HWTSTAMP_FILTER_ALL;
+    cfg.tx_type = HWTSTAMP_TX_OFF;
 
-	strncpy(dev.ifr_name, iface, sizeof(dev.ifr_name));
-	dev.ifr_data = (void *)&cfg;
-	
-	if (ioctl(sd, SIOCSHWTSTAMP, &dev) < 0)
-	{
-		perror("ioctl()");
-		exit(EXIT_FAILURE);
-	}
+    strncpy(dev.ifr_name, iface, sizeof(dev.ifr_name));
+    dev.ifr_data = (void *)&cfg;
 
-	printf("Settings:\n"
-		   "    HWTSTAMP_TX: %d\n"
-		   "    HWTSTAMP_RX: %d\n"
-		   "    Flags:       %d\n",
-		   cfg.tx_type,
-		   cfg.rx_filter,
-		   cfg.flags);
+    if (ioctl(sd, SIOCSHWTSTAMP, &dev) < 0)
+    {
+        perror("ioctl()");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Settings:\n"
+           "    HWTSTAMP_TX: %d\n"
+           "    HWTSTAMP_RX: %d\n"
+           "    Flags:       %d\n",
+           cfg.tx_type,
+           cfg.rx_filter,
+           cfg.flags);
 }
 
 
@@ -50,7 +50,7 @@ static void usage(const char *execname)
 
 int main(int argc, char **argv)
 {
-	int sd;
+    int sd;
     const char *iface = NULL;
 
     if (argc != 2)
@@ -58,15 +58,15 @@ int main(int argc, char **argv)
     else
       iface = argv[1];
     printf("Using interface: %s\n", iface);
-	
-	if ((sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-	{
-	    perror("socket()");
-		exit(EXIT_FAILURE);
-	}
 
-	set_settings(sd, iface);
-	close(sd);
+    if ((sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
 
-	return 0;
+    set_settings(sd, iface);
+    close(sd);
+
+    return 0;
 }
